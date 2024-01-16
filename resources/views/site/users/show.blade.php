9fa730156ca721b20f896a6a051b874eb73c0aeb
admitkok <kokov.alexander@gmail.com>
2024-01-16 09:31:23 +0100

    Some improvements

--- resources/views/site/users/show.blade.php
+++ resources/views/site/users/show.blade.php
@@ -7,37 +7,39 @@
 
     <h2 class="font-bold">Posts by this author</h2>
     <ul class="list-disc ml-5">
-        @foreach($user->posts as $post)
-            <li class="block shadow-sm bg-white hover:shadow-lg rounded-lg grid grid-cols-3 group overflow-hidden">
+        <div class="grid grid-cols-2 items-center">
+            @foreach($user->posts as $post)
+                <li class="block shadow-sm bg-white hover:shadow-lg rounded-lg grid grid-cols-3 group overflow-hidden">
 
-                <a href="{{route('posts.show', ['post' => $post])}}" class="block h-full bg-red-50 rounded-l overflow-hidden">
-                    <img src="{{$post->getImageUrl('preview')}}" class="rounded-l h-full group-hover:scale-110 transition duration-500 cursor-pointer object-cover">
-                </a>
-
-                <div class="col-span-2 p-2">
-                    <div class="flex gap-x-2 mb-2">
-                        @foreach($post->categories as $category)
-                            <a href="{{route('categories.show', ['id' => $category->id])}}" class="uppercase bg-teal-500 hover:bg-teal-600 text-white rounded-full py-1 px-4 text-xs">
-                                {{$category->name}}
-                            </a>
-                        @endforeach
-                    </div>
-
-                    <a href="{{route('posts.show', ['post' => $post])}}" class="">
-                        <h2 class="font-bold text-lg line-clamp-1" >{{$post->title}}</h2>
-                        <span class="">{{$post->author?->name ?? 'UNKNOWN AUTHOR' }}</span>
-                        <span class="text-teal-500 mx-1">|</span>
-                        <span>{{$post->published_at->format('Y-m-d H:i')}}</span>
-
-                        <div class="px-1 pt-2 line-clamp-3">
-                            {{ Str::limit( strip_tags($post->body), 200) }}
-                        </div>
+                    <a href="{{route('posts.show', ['post' => $post])}}" class="block h-full bg-red-50 rounded-l overflow-hidden">
+                        <img src="{{$post->getImageUrl('preview')}}" class="rounded-l h-full group-hover:scale-110 transition duration-500 cursor-pointer object-cover">
                     </a>
 
+                    <div class="col-span-2 p-2">
+                        <div class="flex gap-x-2 mb-2">
+                            @foreach($post->categories as $category)
+                                <a href="{{route('categories.show', ['id' => $category->id])}}" class="uppercase bg-teal-500 hover:bg-teal-600 text-white rounded-full py-1 px-4 text-xs">
+                                    {{$category->name}}
+                                </a>
+                            @endforeach
+                        </div>
 
-                </div>
+                        <a href="{{route('posts.show', ['post' => $post])}}" class="">
+                            <h2 class="font-bold text-lg line-clamp-1" >{{$post->title}}</h2>
+                            <span class="">{{$post->author?->name ?? 'UNKNOWN AUTHOR' }}</span>
+                            <span class="text-teal-500 mx-1">|</span>
+                            <span>{{$post->published_at->format('Y-m-d H:i')}}</span>
 
-            </li>
-        @endforeach
+                            <div class="px-1 pt-2 line-clamp-3">
+                                {{ Str::limit( strip_tags($post->body), 200) }}
+                            </div>
+                        </a>
+
+
+                    </div>
+
+                </li>
+            @endforeach
+        </div>
     </ul>
 </x-site-layout>
