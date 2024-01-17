2477ea66064b947f5c7af6a3f3410227da118aa6
admitkok <kokov.alexander@gmail.com>
2024-01-17 13:23:23 +0100

    Some improvements

--- resources/views/layouts/site.blade.php
+++ resources/views/layouts/site.blade.php
@@ -4,7 +4,7 @@
 <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <title>HTML5 Boilerplate</title>
+    <title>ArtGallery</title>
     <script src="https://cdn.tailwindcss.com"></script>
     <script src="//unpkg.com/alpinejs" defer></script>
     <script src="script.js"></script>
